0d1edf5779a8b54c5d9bf861e88705891b67ada0
EzequielK-source <programacionezequielk@gmail.com>
2022-02-26 13:12:45 -0300

    <test(products)> Refact ProductCreator
    Refact ProductCreator test to verify
    if ProductDAO propertys are the same passed
    for parameters

--- test/apiServices/product/class/product_creator.test.js
+++ test/apiServices/product/class/product_creator.test.js
@@ -1,24 +1,31 @@
 //test deps and config
 const chai = require("chai");
 const chaiAsPromised = require("chai-as-promised")
-chai.use(chaiAsPromised)
+// chai.use(chaiAsPromised)
 const {expect, assert} = chai;
 
 //app deps
 const ProductCreator = require("src/apiServices/product/class/product_creator")
 const ProductDAO = require("src/apiServices/product/class/product_dao")
 describe('ProductCreator test', () => {
-	it('ProductCreator.create return ProductDAO', () => {
-		return expect( ProductCreator.create({
+	it('ProductCreator.create return ProductDAO', async() => {
+		const productFields = {
 			ID_Product: 'product_id',
 			name:"productname",
 			description:"product description",
 			price:1230,
 			barcode:"asdasdasd"
-		}) )
-			   .to
-			   .eventually
-			   .be
-			   .an.instanceof(ProductDAO)
+		}
+		const productCreated = await ProductCreator.create(productFields);
+		//verify if an ProductDAO intance
+		expect(productCreated)
+			.to
+			.be.instanceof(ProductDAO);
+		it('productCreated have same propertys passed for params', () => {
+			expect(productCreated)
+				.to
+				.be
+				.deep.equal(productFields);
+		});
 	});
 });
