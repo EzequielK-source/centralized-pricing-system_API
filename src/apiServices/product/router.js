742b452df93229fda9b0b6ce89d156c3f9600656
EzequielK-source <programacionezequielk@gmail.com>
2022-02-27 20:07:14 -0300

    <feat> Add /products/:barcode
    Add /products/:barcode and GET request method
    that implements Product Finder with the method
    findBy Barcode for return Product.

--- src/apiServices/product/router.js
+++ src/apiServices/product/router.js
@@ -3,6 +3,7 @@ const productRouter = express.Router();
 
 const ProductFieldsVerificator = require('./class/product_fields_verificator');
 const ProductCreator = require('./class/product_creator');
+const ProductFinder = require('./class/product_finder');
 productRouter.route("/")
 	.get((req,res)=>{
 		return res.sendStatus(200)
@@ -14,9 +15,7 @@ productRouter.route("/")
 			const {
 				Name,
 				Price,
-				Barcode,
-				Description
-			} = await ProductCreator.create(req.body)
+				Barcode,Description} = await ProductCreator.create(req.body)
 			return res.status(201).json({
 				status:"product created",
 				product:{
@@ -32,4 +31,14 @@ productRouter.route("/")
 		}
 	})
 
+productRouter.get("/:barcode", async (req,res)=>{
+	try{
+		res.set('Content-Type', 'application/json');
+		const barcode = req.params.barcode;
+		const product = await ProductFinder.findByBarcode(barcode)
+		return res.status(200).json(product)
+	}catch(err){
+		return res.status(400).json(err);
+	}
+})
 module.exports = productRouter
