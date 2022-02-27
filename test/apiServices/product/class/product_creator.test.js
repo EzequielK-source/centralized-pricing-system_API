4eb2d35e6562ec4a7435783ba6b6a48c1d97ac5e
EzequielK-source <programacionezequielk@gmail.com>
2022-02-27 17:32:00 -0300

    <test> Add 'Product has persisted' test
    'Product has persisted' verify in DB
    if product was persited by ProductCreator.
    
    Changes:
    We replace the first letter of all Product
    Fields properties with uppercase.
    
    Add before hook for 'MissingProductField' test
    for delete all rows from table.

--- test/apiServices/product/class/product_creator.test.js
+++ test/apiServices/product/class/product_creator.test.js
@@ -6,18 +6,91 @@ const {expect, assert} = chai;
 
 //modules
 const {validate: uuidValidate} = require('uuid')
+const { QueryTypes } = require('sequelize');
 //app deps
+const {sequelize} =require('src/services/sequelize/index');
 const ProductCreator = require("src/apiServices/product/class/product_creator")
 const ProductDAO = require("src/apiServices/product/class/product_dao")
 const MissingProductField = require("src/apiServices/product/exception/missing-product-field")
 describe('ProductCreator test', () => {
+
+	describe('Missing any productField throw MissingProductField', () => {
+		it('Missing ID_Product not throw', async() => {
+			const productFields = {
+				Name:"productName",
+				Description:"product Description",
+				Price:1230,
+				Barcode:"asdasdasd"
+			}
+			const productCreated = await ProductCreator.create(productFields);
+			expect(productCreated)
+				.to
+				.have
+				.property("ID_Product")
+				.not
+				.be
+				.empty;
+
+			expect(uuidValidate(productCreated.ID_Product)).to.be.true;
+		});
+		it('Missing Name throw', () => {
+			const productFields = {
+				Description:"product Description",
+				Price:1230,
+				Barcode:"asdasdasd"
+			}
+
+			return expect(ProductCreator.create(productFields))
+				.to
+				.eventually
+				.rejectedWith(MissingProductField, "Missing Name field")
+		});
+		it('Missing Description throw', () => {
+			const productFields = {
+				Name:"Named",
+				Price:1230,
+				Barcode:"asdasdasd"
+			}
+
+			return expect(ProductCreator.create(productFields))
+				.to
+				.eventually
+				.rejectedWith(MissingProductField, "Missing Description field")
+		});
+		it('Missing Price throw', () => {
+			const productFields = {
+				Name:"Named",
+				Description:"product Description",
+				Barcode:"asdasdasd"
+			}
+
+			return expect(ProductCreator.create(productFields))
+				.to
+				.eventually
+				.rejectedWith(MissingProductField, "Missing Price field")
+		});
+		it('Missing Barcode throw', () => {
+			const productFields = {
+				Name:"Named",
+				Description:"product Description",
+				Price:1230
+			}
+
+			return expect(ProductCreator.create(productFields))
+				.to
+				.eventually
+				.rejectedWith(MissingProductField, "Missing Barcode field")
+		});
+		before(async()=>{
+			await sequelize.query(`DELETE FROM Products`, { type: QueryTypes.DELETE });
+		})
+	});
 	it('ProductCreator.create return ProductDAO', async() => {
 		const productFields = {
-			ID_Product: 'product_id',
-			name:"productname",
-			description:"product description",
-			price:1230,
-			barcode:"asdasdasd"
+			Name:"productName",
+			Description:"product Description",
+			Price:1230,
+			Barcode:"asdasdasd"
 		}
 		const productCreated = await ProductCreator.create(productFields);
 		//verify if an ProductDAO intance
@@ -29,76 +102,24 @@ describe('ProductCreator test', () => {
 				.be
 				.deep.equal(productFields);
 	});
-	describe('Missing any productField throw MissingProductField', () => {
-		it('Missing ID_Product not throw', async() => {
-			const productFields = {
-				name:"productname",
-				description:"product description",
-				price:1230,
-				barcode:"asdasdasd"
-			}
-			const productCreated = await ProductCreator.create(productFields);
-			expect(productCreated)
-				.to
-				.have
-				.property("ID_Product")
-				.not
-				.be
-				.empty;
-				
-			expect(uuidValidate(productCreated.ID_Product)).to.be.true;
-		});
-		it('Missing name throw', () => {
-			const productFields = {
-				ID_Product: 'product_id',
-				description:"product description",
-				price:1230,
-				barcode:"asdasdasd"
-			}
+	it('Product has persisted', async () => {
+		const productFields = {
+			ID_Product: "testing_id",
+			Name:"testing_Name",
+			Price:123,
+			Barcode:"testing_Barcode",
+			Description:"testing_Description",
+		}
+		await ProductCreator.create(productFields);
+		const selectQuery = `SELECT * FROM Products WHERE BINARY ID_Product='${productFields.ID_Product}'`
+		const product = await sequelize.query(selectQuery, { type: QueryTypes.SELECT });
 
-			return expect(ProductCreator.create(productFields))
-				.to
-				.eventually
-				.rejectedWith(MissingProductField, "Missing name field")
-		});
-		it('Missing description throw', () => {
-			const productFields = {
-				ID_Product: 'product_id',
-				name:"named",
-				price:1230,
-				barcode:"asdasdasd"
-			}
 
-			return expect(ProductCreator.create(productFields))
-				.to
-				.eventually
-				.rejectedWith(MissingProductField, "Missing description field")
-		});
-		it('Missing price throw', () => {
-			const productFields = {
-				ID_Product: 'product_id',
-				name:"named",
-				description:"product description",
-				barcode:"asdasdasd"
-			}
-
-			return expect(ProductCreator.create(productFields))
-				.to
-				.eventually
-				.rejectedWith(MissingProductField, "Missing price field")
-		});
-		it('Missing barcode throw', () => {
-			const productFields = {
-				ID_Product: 'product_id',
-				name:"named",
-				description:"product description",
-				price:1230
-			}
-
-			return expect(ProductCreator.create(productFields))
-				.to
-				.eventually
-				.rejectedWith(MissingProductField, "Missing barcode field")
-		});
+		expect(product)
+			.to
+			.have.property('length').equal(1)
 	});
+	before(async()=>{
+		await sequelize.query(`DELETE FROM Products`, { type: QueryTypes.DELETE });
+	})
 });
