88bd1be80861c5fe1f8d6563f73a3ee2d6b37e13
EzequielK-source <programacionezequielk@gmail.com>
2022-02-27 18:28:54 -0300

    <test> We add the test of the other ProductFields
    We add the test of the other ProductFields
    to verify that they return 400 when
    they are missing.

--- test/apiServices/product/router.test.js
+++ test/apiServices/product/router.test.js
@@ -11,13 +11,55 @@ const request = chai.request;
 describe('API /products router', () => {
 	describe('product post request', () => {
 		describe('missing any body param response status 400', () => {
-			it('missing Name throw error', (done) => {
+			it('missing Name send 400 status', (done) => {
 				request(app)
 					.post("/products")
 					.send({
 						Description: "description",
 						Barcode: "barcode",
 						Price: "Price"
+					})
+					.end((err,res)=>{
+						if(err) done(err)
+						expect(res).to.have.status(400)
+						done()
+					})
+			});
+			it('missing Description send 400 status', (done) => {
+				request(app)
+					.post("/products")
+					.send({
+						Name: "description",
+						Barcode: "barcode",
+						Price: "Price"
+					})
+					.end((err,res)=>{
+						if(err) done(err)
+						expect(res).to.have.status(400)
+						done()
+					})
+			});
+			it('missing Price send 400 status', (done) => {
+				request(app)
+					.post("/products")
+					.send({
+						Name: "description",
+						Barcode: "barcode",
+						Description: "Price"
+					})
+					.end((err,res)=>{
+						if(err) done(err)
+						expect(res).to.have.status(400)
+						done()
+					})
+			});
+			it('missing Barcode send 400 status', (done) => {
+				request(app)
+					.post("/products")
+					.send({
+						Name: "description",
+						Price: "barcode",
+						Description: "Price"
 					})
 					.end((err,res)=>{
 						if(err) done(err)
