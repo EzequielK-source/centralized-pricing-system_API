2c3b93bdd949abc32539244887ac5177ea20638a
EzequielK-source <programacionezequielk@gmail.com>
2022-02-27 18:23:29 -0300

    <feat> Add POST method for /products route
    We implement ProductFieldVerificator in POST method
    route for break flow and return 400 if missing
    any ProductField.

--- src/apiServices/product/router.js
+++ src/apiServices/product/router.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const productRouter = express.Router();
 
-productRouter.get("/",(req,res)=>{
-	return res.sendStatus(200)
-})
+const ProductFieldsVerificator = require('./class/product_fields_verificator');
+
+productRouter.route("/")
+	.get((req,res)=>{
+		return res.sendStatus(200)
+	})
+	.post((req,res)=>{
+		try{
+			new ProductFieldsVerificator(req.body)
+		}catch(err){
+			return res.sendStatus(400)
+		}
+	})
+
 module.exports = productRouter
