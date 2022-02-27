8806310c36d550bf6fbecce39498a2f39c76ef1c
EzequielK-source <programacionezequielk@gmail.com>
2022-02-27 18:53:40 -0300

    <feat>implement ProductCreator to POST /products
    Now ProductRouter in POST Method persit Product
    using ProductCreator.

--- src/apiServices/product/router.js
+++ src/apiServices/product/router.js
@@ -2,14 +2,31 @@ const express = require("express");
 const productRouter = express.Router();
 
 const ProductFieldsVerificator = require('./class/product_fields_verificator');
-
+const ProductCreator = require('./class/product_creator');
 productRouter.route("/")
 	.get((req,res)=>{
 		return res.sendStatus(200)
 	})
-	.post((req,res)=>{
+	.post(async(req,res)=>{
 		try{
+			res.set('Content-Type', 'application/json');
 			new ProductFieldsVerificator(req.body)
+			const {
+				Name,
+				Price,
+				Barcode,
+				Description
+			} = await ProductCreator.create(req.body)
+			return res.status(201).json({
+				status:"product created",
+				product:{
+					Name,
+					Price,
+					Barcode,
+					Description
+				}
+			})
+
 		}catch(err){
 			return res.sendStatus(400)
 		}
