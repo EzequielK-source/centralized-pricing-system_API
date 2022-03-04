78297718972f21282f8f9899f49df479adaaf241
EzequielK-source <programacionezequielk@gmail.com>
2022-03-03 21:05:34 -0300

    <test> Add test for ProductFinder
    Verify if ProductFinder.findByBarcode method
    throw error when pass unregistered barcode

--- test/apiServices/product/class/product_finder.test.js
+++ test/apiServices/product/class/product_finder.test.js
@@ -12,6 +12,7 @@ const {sequelize} =require('src/services/sequelize/index');
 const ProductCreator = require('src/apiServices/product/class/product_creator');
 const ProductFinder = require('src/apiServices/product/class/product_finder');
 const ProductDAO = require('src/apiServices/product/class/product_dao');
+const UnregisteredBarcode = require("src/apiServices/product/exception/unregistered-barcode")
 describe('ProductFinder test', () => {
 	const product = {
 		ID_Product: "product_test_id",
@@ -24,15 +25,27 @@ describe('ProductFinder test', () => {
 		/* create one temporal product */
 		await ProductCreator.create(product);
 	})
-	it('Find Product by Barcode', async() => {
-		const product_findend = await ProductFinder.findByBarcode(product.Barcode)
-		expect(product_findend)
-			.to
+	describe('findProductByBarcode test', ()=>{
+		it('Find Product by Barcode', async() => {
+			const product_findend = await ProductFinder.findByBarcode(product.Barcode)
+			expect(product_findend)
+				.to
+				.be
+				.instanceof(ProductDAO)
+			expect(product_findend)
+				.to
+				.be
+				.deep.equal(product)
+		});
+		it('Searching for an unregistered barcode throws an error', () => {
+			return expect(
+				ProductFinder.findByBarcode("nonExistBarcode")
+			)
+			.eventually
 			.be
-			.instanceof(ProductDAO)
-		expect(product_findend)
-			.to
-			.be
-			.deep.equal(product)
-	});
+			.rejectedWith(UnregisteredBarcode, "Barcode not registered in a product")
+
+		});
+	})
+
 });
