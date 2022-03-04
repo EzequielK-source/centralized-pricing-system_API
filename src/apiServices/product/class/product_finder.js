8dfb2a121d595a3ba6ae10c0426aa0a6bab0c8b3
EzequielK-source <programacionezequielk@gmail.com>
2022-03-03 21:09:58 -0300

    <feat> Throw error when miss Product
    Now, when findByBarcode method not find
    one product registered with barcode, throw
    UnregisteredBarcode error.

--- src/apiServices/product/class/product_finder.js
+++ src/apiServices/product/class/product_finder.js
@@ -1,9 +1,12 @@
 const {product: ProductModel} = require('src/services/sequelize/index');
 const ProductDAO = require('./product_dao');
+const UnregisteredBarcode = require('../exception/unregistered-barcode');
 module.exports = class ProductFinder {
 	static async findByBarcode(Barcode){
 		/**
 			* Find one product by barcode and return ProductDAO
+			* if there is no registered product with
+			  the barcode it throws an error
 			*@param Barcode STRING
 		**/
 		const productFindend = await ProductModel.findOne({
@@ -11,6 +14,8 @@ module.exports = class ProductFinder {
 				Barcode
 			}
 		})
+
+		if(productFindend == null) throw new UnregisteredBarcode();
 
 		return Promise.resolve(new ProductDAO(productFindend))
 	}
