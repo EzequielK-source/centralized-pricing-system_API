351f3bbeaf2eba6af5e81bde2a8a8c551f53645a
EzequielK-source <programacionezequielk@gmail.com>
2022-03-14 21:33:55 -0300

    <feat> Delete modifyByPK method

--- src/apiServices/product/class/product_modifier.js
+++ src/apiServices/product/class/product_modifier.js
@@ -3,25 +3,6 @@ const UnregisteredBarcode = require('../exception/unregistered-barcode');
 const ProductFieldsVerificator = require("./product_fields_verificator")
 const ProductDAO = require("./product_dao")
 module.exports = class ProductModifier {
-	static async modifyByPK(ID_Product, new_fields){
-		/**
-			* Search Product by ID and modify then
-			  as long as dont try edit ID_Product
-			  or barcode
-
-			 *@param ID_Product STRING
-			 *@param new_fields OBJECT
-	    **/
-		this.validateNewFields(new_fields)
-		const product = await ProductModel.findByPk(ID_Product);
-		if(!product) throw new UnregisteredID();
-		for(const property in new_fields){
-			product[property] = new_fields[property]
-		}
-
-		await product.save();
-		return Promise.resolve(new ProductDAO(product))
-	}
 	static async modifyByBarcode(Barcode, new_fields){
 		/**
 			* Search Product by Barcode and modify then
