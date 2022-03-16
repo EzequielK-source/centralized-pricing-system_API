0a80cf95fae492cddf0498b949a660340009ab8f
EzequielK-source <programacionezequielk@gmail.com>
2022-03-16 20:27:22 -0300

    <feat> Router '/users' route with userRouter

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const productRouter = require("src/apiServices/product/router")
+const userRouter = require("src/apiServices/user/router")
 const indexRouter = express.Router();
 indexRouter.use("/products",productRouter)
+indexRouter.use("/users",userRouter)
 
 module.exports = indexRouter
