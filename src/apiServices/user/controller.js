d3bfb9f63a9461dfd11186d845f6de227654bb2a
EzequielK-source <programacionezequielk@gmail.com>
2022-03-16 21:01:52 -0300

    <feat> Create createUser method for UserController and implement UserCreator

--- src/apiServices/user/controller.js
+++ src/apiServices/user/controller.js
@@ -1,7 +1,15 @@
 const userController = {};
-
+const UserCreator = require('./class/user_creator')
 userController.getUserRouteStatus = async(req,res)=>{
 	return res.sendStatus(200)
 }
 
+userController.createUser = async (req,res)=>{
+	try{
+		await UserCreator.create(req.body)
+		return res.sendStatus(201)
+	}catch(err){
+		return res.sendStatus(400)
+	}
+}
 module.exports = userController;
